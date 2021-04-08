7d63fba14ca2b9c337af531fa6186692e478a327
hiroki kameyama <hkame@jp.ibm.com>
2021-04-09 06:54:45 +0900

    modify CardGameforTwo

--- practice/CardGameforTwo/Main.java
+++ practice/CardGameforTwo/Main.java
@@ -4,31 +4,28 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new  Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Integer[] intArray = new Integer[N];
-
-        for(int i=0;i<intArray.length;i++){
-            intArray[i] = sc.nextInt();
+        Integer[] A= new Integer[N];
+        for(int i=0;i<N;i++){
+            A[i]=sc.nextInt();
         }
-        Arrays.sort(intArray,Collections.reverseOrder());
 
-        int A = 0;
-        int B = 0;
-        int count = 1;
-        for(int j:intArray){
-            if(count%2==1){
-                A += j;
+        Arrays.sort(A,Collections.reverseOrder());
+        System.out.println(Arrays.asList(A));
+
+        int sumA=0;
+        int sumB=0;
+        for(int i=0;i<A.length;i++){
+            if(i%2==0){
+                sumA += A[i];
             }else{
-                B += j;
+                sumB += A[i];
             }
-            count++;
         }
-        int diff = A - B;
-        System.out.println(diff);
+        System.out.println(sumA - sumB);
         sc.close();
-        
     }
 }
